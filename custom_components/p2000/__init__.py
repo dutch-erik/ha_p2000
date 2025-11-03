412b16f2b92006813f7f500ad24c08bca2983f63
Erik <62548287+dutch-erik@users.noreply.github.com>
2025-11-03 15:55:45 +0100

    Update __init__.py

--- custom_components/p2000/__init__.py
+++ custom_components/p2000/__init__.py
@@ -1,5 +1,4 @@
 """Init file for P2000 integration."""
-
 from __future__ import annotations
 import logging
 from homeassistant.config_entries import ConfigEntry
@@ -10,13 +9,14 @@ from .const import DOMAIN
 _LOGGER = logging.getLogger(__name__)
 
 async def async_setup(hass: HomeAssistant, config: dict):
+    """Laat legacy YAML-configuratie toe."""
     return True
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    )
+    """Setup P2000-platform via config entry (UI-config)."""
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Wanneer een UI-entry wordt verwijderd."""
     return await hass.config_entries.async_forward_entry_unload(entry, "sensor")
