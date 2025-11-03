c71bce654f5f7e79dbad4b359bbae791d9cce12e
Erik <62548287+dutch-erik@users.noreply.github.com>
2025-11-03 15:22:31 +0100

    Update __init__.py

--- custom_components/p2000/__init__.py
+++ custom_components/p2000/__init__.py
@@ -1,1 +1,22 @@
-"""The p2000 sensor integration."""
+"""Init file for P2000 integration."""
+
+from __future__ import annotations
+import logging
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    )
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    return await hass.config_entries.async_forward_entry_unload(entry, "sensor")
